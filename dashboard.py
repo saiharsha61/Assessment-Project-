7a805b5d454a42de135f31d43bf63aa53487e475
saiharsha61 <121154104+saiharsha61@users.noreply.github.com>
2025-09-15 11:05:15 +0530

    Create DashboardService class with resource methods

---
+++ dashboard.py
@@ -0,0 +1,13 @@
+# dashboard.py
+# Smart Dashboard module
+
+class DashboardService:
+    def get_top_rated_resources(self):
+        """Return top-rated resources."""
+        pass
+    def get_most_downloaded(self):
+        """Return most-downloaded resources."""
+        pass
+    def get_recommendations(self, user):
+        """Return personalized recommendations for a user."""
+        pass
